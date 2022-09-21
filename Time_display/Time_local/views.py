b3cc41a03df09211d7ebe00bf78154821106b2cd
ali Tamrawe <Ali.h.tamrawe@gmail.com>
2022-09-21 11:17:32 +0300

    final trick

--- Time_display/Time_local/views.py
+++ Time_display/Time_local/views.py
@@ -4,6 +4,7 @@ from time import gmtime, strftime
 
 def index(request):
     context = {
+        
         "time": strftime("%Y-%m-%d %H:%M %p ", gmtime())
     }
     return render(request,'index.html', context)
