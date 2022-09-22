e12758b2140b85b13a89f6c89ea9b810fdb9fc47
ali Tamrawe <Ali.h.tamrawe@gmail.com>
2022-09-22 09:26:20 +0300

    completed

--- Counter/count_app/views.py
+++ Counter/count_app/views.py
@@ -3,17 +3,16 @@ from django.shortcuts import render,redirect,HttpResponse
 
 
 def index(request):
-    if request.session == 'test':
-        # count=request.session['count']
-        # count+=1
-        # request.session['test']=count
-        # request.session['count']=request.session['test']
-        # request.session.save()
-        pass
+    
+    if request.GET =='plus' :
+        request.session['test'] +=2
+        request.session.save()
+        if request.session == 'test':
+            request.session['test']=0
+            request.session.save()
     else:
         request.session['test'] +=1
         request.session.save()
-    
     return render(request,'index.html')
 
 def destroy(request):
