126cce2dfcaa77f7f2e156b0a6187a5286b27f2e
ali Tamrawe <Ali.h.tamrawe@gmail.com>
2022-09-22 13:19:23 +0300

    update

--- orms_project/orms_project/urls.py
+++ orms_project/orms_project/urls.py
@@ -2,5 +2,5 @@ from xml.etree.ElementInclude import include
 from django.urls import path
 
 urlpatterns = [
-    path('',include('user_app.urls')),
+    # path('',include('user_app.urls')),
 ]
--- orms_project/user_app/models.py
+++ orms_project/user_app/models.py
@@ -2,13 +2,27 @@ from platform import release
 from turtle import update
 from django.db import models
 
-class movie(models.Model):
-    title = models.TextField(max_length=45)
-    description = models.TextField()
-    release_date  = models.DateTimeField()
-    duration = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+
+class Movie(models.Model):
+        title = models.TextField(max_length=45)
+        description = models.TextField()
+        release_date  = models.DateTimeField()
+        duration = models.IntegerField()
+        created_at = models.DateTimeField(auto_now_add=True)
+        update_at = models.DateTimeField(auto_now=True)
+
+
+
+
+class Users(models.Model):
+        first_name = models.TextField(max_length=45)
+        last_name= models.TextField(max_length=45)
+        email_address  = models.EmailField()
+        age = models.IntegerField()
+        created_at = models.DateTimeField(auto_now_add=True)
+        update_at = models.DateTimeField(auto_now=True)
+        def __str__(self):
+                return f"<Users object: {self.first_name} ({self.last_name})>"
 
 
 
