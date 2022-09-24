4097fdb47eb33c8e1a7a67bf82e3fa98d0f90c39
ali Tamrawe <Ali.h.tamrawe@gmail.com>
2022-09-25 00:29:45 +0300

    new changes

--- Greater_number/greater_number_app/views.py
+++ Greater_number/greater_number_app/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render
+import random 	                 
+
 
 def callculate(request):
-    pass
+    color ='white'
+    result = ' '
+    number = random.randint(1, 100) 
+    if request.GET:
+        print(request.GET)
+        answe = (request.GET['answer'])
+        answer = int(answe)
+        if answer == number:
+            color = 'green'
+            result =f"{number} was the <br> number!"
+        if answer > number :
+            color = 'red'
+            result = "Too High !"
+        if answer < number :
+            color = 'red'
+            result = "Too Low !"
+    context = {
+        'color':color,
+        'result':result,
+    }
+    return render(request,'index.html',context)
+
