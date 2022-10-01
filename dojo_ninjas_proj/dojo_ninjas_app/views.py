42005fb612be652f4177f9f39407c53ffb845b21
ali Tamrawe <Ali.h.tamrawe@gmail.com>
2022-10-01 15:05:57 +0300

    done with dojo and ninja

--- dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -10,25 +10,25 @@ def root(request):
 
 def insert(request):
     print(request.POST.get('dojo'))
-   
-    if request.POST.get('dojo') =='add1' :
-        errors = Ninjas.objects.validate_ninja(request.POST)
-        if errors > 0:
+    errors = Ninjas.objects.validate_ninja(request.POST)
+    if not errors :
+        messages.error(request, "Successful validate")
+        if request.POST.get('dojo') =='add1' :
             name=request.POST.get('name_from')
             print("submiting ...........")
             city= request.POST.get('city_from')
             state = request.POST.get('state_from')
             desc = request.POST.get('desc_from')
-            Dojos.objects.create(name=name,city=city,state=state,)
+            Dojos.objects.create(name=name,city=city,state=state,desc=desc)
         elif request.POST.get('dojo') =='addt':
             first_name=request.POST.get('first_name_from')
             print("submiting ...........")
             last_ame= request.POST.get('last_name_from')
             dojoi = request.POST.get('ids')
             Ninjas.objects.create(first_name=first_name,last_ame=last_ame,Dojo=Dojos.objects.get(id=dojoi))
-        else:
-            for key, value in errors.items():
-                messages.error(request, value)
+    else:
+        for key, value in errors.items():
+            messages.error(request, value)
     
     context = {
     "all_the_dojo": Dojos.objects.values(),
