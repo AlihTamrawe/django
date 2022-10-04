18a7dfe0cf34ad2d7c4c8af6faf91486c6124274
ali Tamrawe <Ali.h.tamrawe@gmail.com>
2022-10-04 10:49:44 +0300

    last version

--- The_Wall/wall_app/views.py
+++ The_Wall/wall_app/views.py
@@ -19,7 +19,7 @@ def index(request):
         comm =  request.POST.get('comment_inserted')
         Comments.objects.create(comment=comm,commander=User.objects.get(id=request.session['id']),reply=Messages.objects.get(id=id_for_m))
     context ={
-        'id1':request.session['id'],
+        'id1':User.objects.get(id=request.session['id']),
         'Messages':Messages.objects.all(),
         'Comments':Comments.objects.all(),
         # 'owner_message':
